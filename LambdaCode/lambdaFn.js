23a2fddfa8ff4d3449c5a9974527a1ee24a22523
abbaszain <zainuabbas7@gmail.com>
2022-03-29 18:14:59 +0530

    Update lambdaFn.js

--- LambdaCode/lambdaFn.js
+++ LambdaCode/lambdaFn.js
@@ -1,8 +1,18 @@
+const aws = require('aws-sdk');
+const ec2 = new aws.EC2({apiVersion: '2016-11-15'});
+
 exports.handler = async (event) => {
-    // TODO implement
+    let resp;
+    ec2.describeAvailabilityZones(params, (err, data) => {
+    if (err) {
+    console.log("Error", err);
+  } else {
+    resp = data.AvailabilityZones;
+  }
+});
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello World!'),
+        body: JSON.stringify(resp)
     };
     return response;
 };
